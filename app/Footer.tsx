afcfc63179d22f24bf441f5e7a8586121f51c380
r4bb1t <r4bb1t.github@gmail.com>
2025-02-20 16:50:29 +0900

    ✨ Feat: Create landing page with mascot and cute font

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex w-full shrink-0 items-center justify-center px-4 py-2 text-sm">
+    <footer className="flex w-full shrink-0 items-center justify-center px-6 py-2 text-sm">
       <div className="font-semibold">2025 고려대학교 동아리박람회 @KUCC</div>
     </footer>
   );
--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="absolute top-0 z-10 flex h-16 w-full max-w-md items-center justify-between px-4">
+    <header className="absolute top-0 z-10 flex h-16 w-full max-w-md items-center justify-between px-6">
       <div className="font-bold">나에게 맞는 IT 직군은?</div>
       <img src="/mascot.png" className="size-8 shrink-0 object-contain" />
     </header>
