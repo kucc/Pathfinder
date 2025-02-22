d5d0a7b12009044c83177412eb734615b16350e3
r4bb1t <r4bb1t.github@gmail.com>
2025-02-22 21:24:11 +0900

    🐛 Fix: Correct type parameter for Result page async function

--- app/result/[type]/page.tsx
+++ app/result/[type]/page.tsx
@@ -1,6 +1,10 @@
 import { MBTI } from "@/app/types";
 
-export default async function Result({ params }: { params: { type: MBTI } }) {
+export default async function Result({
+  params,
+}: {
+  params: Promise<{ type: MBTI }>;
+}) {
   const { type } = await params;
   return <div>{type}</div>;
 }
