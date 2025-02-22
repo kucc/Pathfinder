4447a5c2707ea71cecbfeb761a195dc629a765b7
r4bb1t <r4bb1t.github@gmail.com>
2025-02-22 22:40:08 +0900

    ✨ Feat: Add Contacts component to Result page and improve layout

--- app/result/[type]/page.tsx
+++ app/result/[type]/page.tsx
@@ -1,6 +1,7 @@
 import { CAREER } from "@/app/constant";
 import { activityToKorean } from "@/public/utils/toKorean";
 import { notFound } from "next/navigation";
+import Contacts from "./Contacts";
 
 export default async function Result({
   params,
@@ -15,35 +16,44 @@ export default async function Result({
   }
 
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-4 p-6">
-      <div className="flex flex-col items-center gap-1">
-        {result.emoji}
-        <p className="text-center text-sm font-bold text-primary">
-          {result.summary}
+    <div className="flex flex-col justify-between gap-4 pt-12">
+      <div className="flex h-full flex-col items-center justify-center gap-4 p-6">
+        <div className="flex flex-col items-center gap-1">
+          {result.emoji}
+          <p className="text-center text-sm font-bold text-primary">
+            {result.summary}
+          </p>
+          <h1 className="font-cute text-3xl font-black">{result.title}</h1>
+        </div>
+
+        <div className="w-full max-w-64">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={result.image}
+            alt={result.title}
+            className="object-contain"
+          />
+        </div>
+
+        <p className="break-keep rounded-lg bg-base-200 px-4 py-2 text-center leading-relaxed">
+          <BoldText text={result.description} />
         </p>
-        <h1 className="font-cute text-3xl font-black">{result.title}</h1>
-      </div>
 
-      <div className="w-full max-w-64">
-        <img src={result.image} alt={result.title} className="object-contain" />
-      </div>
-
-      <p className="break-keep rounded-lg bg-base-200 px-4 py-2 text-center leading-relaxed">
-        <BoldText text={result.description} />
-      </p>
-
-      <div className="flex w-full flex-col items-center">
-        <div className="divider font-bold text-primary">
-          KUCC에서 진행된 {result.title} 관련 활동
-        </div>
-
-        <div className="flex items-center justify-start gap-2 text-center text-sm font-bold">
-          <div className="badge badge-primary badge-outline">
-            {activityToKorean(result.activity.type)}
+        <div className="flex w-full flex-col items-center">
+          <div className="divider font-bold text-primary">
+            KUCC에서 진행된 {result.title} 관련 활동
           </div>
-          {result.activity.title}
+
+          <div className="flex items-center justify-start gap-2 text-center text-sm font-bold">
+            <div className="badge badge-primary badge-outline">
+              {activityToKorean(result.activity.type)}
+            </div>
+            {result.activity.title}
+          </div>
         </div>
       </div>
+
+      <Contacts />
     </div>
   );
 }
