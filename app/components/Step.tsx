a142ae6f5d6308ab55c34e297a4dd2ecc43864f1
r4bb1t <r4bb1t.github@gmail.com>
2025-02-22 23:59:03 +0900

    ✨ Feat: Add optional message display in MBTI test step

--- app/components/Step.tsx
+++ app/components/Step.tsx
@@ -10,9 +10,18 @@ export default function Step({
 }) {
   return (
     <div className="flex h-full shrink flex-col items-center justify-between">
-      <p className="break-keep text-center text-xl font-bold">
-        {question.question}
-      </p>
+      <div className="flex w-full flex-col gap-4">
+        <p className="break-keep text-center text-xl font-bold">
+          {question.question}
+        </p>
+        {question.message && (
+          <div className="chat chat-start">
+            <div className="chat-bubble chat-bubble-primary max-w-none">
+              {question.message}
+            </div>
+          </div>
+        )}
+      </div>
 
       <div className="flex w-full flex-col gap-4">
         {question.options.map((option, index) => (
