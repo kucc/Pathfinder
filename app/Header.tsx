5e3321c1f48419c9e336dfe98b4a51b9aced3a31
r4bb1t <r4bb1t.github@gmail.com>
2025-02-22 22:16:05 +0900

    ✨ Feat: Make header title a home link

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="absolute top-0 z-10 flex h-16 w-full max-w-md items-center justify-between px-6">
-      <div className="font-cute font-bold">나에게 맞는 IT 직군은?</div>
+      <Link href="/" className="font-cute font-bold">
+        나에게 맞는 IT 직군은?
+      </Link>
       <img src="/mascot.png" className="size-8 shrink-0 object-contain" />
     </header>
   );
