05ec10ccf0b929de26639ca9a003f917ac20ff54
r4bb1t <r4bb1t.github@gmail.com>
2025-02-22 22:47:24 +0900

    🐛 Fix: Update favicon configuration in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Header from "./Header";
 export const metadata: Metadata = {
   title: "나에게 맞는 IT 직군은?",
   description: "2025 고려대학교 동아리박람회 @KUCC",
+  icons: {
+    icon: [{ url: "/favicon.ico" }],
+  },
 };
 
 export default function RootLayout({
@@ -15,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className="flex h-screen w-full flex-col items-center justify-center bg-base-200">
         <Header />
         <div className="flex h-screen w-full max-w-md flex-col justify-between bg-white shadow-md">
